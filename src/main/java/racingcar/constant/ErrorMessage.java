a8b4dd25f09d5498848924445c5e8395c04a5a92
gumguard <gawonlee0712@gmail.com>
2025-10-27 15:37:49 +0900

    feat(errorMessage): inputValidator 관련 에러메세지

--- src/main/java/racingcar/constant/ErrorMessage.java
+++ src/main/java/racingcar/constant/ErrorMessage.java
@@ -5,7 +5,10 @@ public enum ErrorMessage {
     INVALID_CAR_BLANK_NAME("차의 이름은 비울 수 없습니다: "),
 
     INVALID_CARS_BLANK_CAR_NAME("차 이름 리스트가 비었거나 null입니다: "),
-    INVALID_CARS_DUPLICATE_CAR_NAME("차 이름 리스트에 중복되는 값이 존재합니다: ");
+    INVALID_CARS_DUPLICATE_CAR_NAME("차 이름 리스트에 중복되는 값이 존재합니다: "),
+
+    INVALID_ROUND_COUNT_NUMBER("시도 횟수는 유효한 숫자여야 합니다: "),
+    INVALID_ROUND_COUNT_POSITIVE("시도 횟수는 1 이상의 양수여야 합니다: ");
 
     private final String message;
 
