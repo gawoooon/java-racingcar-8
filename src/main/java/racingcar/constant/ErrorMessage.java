243c77bbd392b0cd1ddd51c13e03941b1b4aea4a
gumguard <gawonlee0712@gmail.com>
2025-10-27 14:50:55 +0900

    feat(errorMessage): Cars 관련 에러메세지 정리

--- src/main/java/racingcar/constant/ErrorMessage.java
+++ src/main/java/racingcar/constant/ErrorMessage.java
@@ -2,7 +2,10 @@ package racingcar.constant;
 
 public enum ErrorMessage {
     INVALID_CAR_NAME_LENGTH("차의 이름은 공백 포함 5자 이하여야 합니다: "),
-    INVALID_CAR_NAME_BLANK("차의 이름은 비울 수 없습니다: ");
+    INVALID_CAR_BLANK_NAME("차의 이름은 비울 수 없습니다: "),
+
+    INVALID_CARS_BLANK_CAR_NAME("차 이름 리스트가 비었거나 null입니다: "),
+    INVALID_CARS_DUPLICATE_CAR_NAME("차 이름 리스트에 중복되는 값이 존재합니다: ");
 
     private final String message;
 
